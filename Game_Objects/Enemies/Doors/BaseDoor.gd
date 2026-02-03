4a428bc7da6b329bf9168697d15299a99f73962d
Trinity the third of the Xon's. <gameit506@gmail.com>
2026-02-04 01:27:54 +0300

    Entrance Scene creation

--- Game_Objects/Enemies/Doors/BaseDoor.gd
+++ Game_Objects/Enemies/Doors/BaseDoor.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
 @export var level: String
+@export var visibleSprite: bool = true
+@onready var door = $Door
+
+func _ready():
+	if not visibleSprite:
+		door.visible = false
+
+
 func _on_pickup_has_picked(body):
 	if level != '':
 		get_tree().change_scene_to_file(level)
