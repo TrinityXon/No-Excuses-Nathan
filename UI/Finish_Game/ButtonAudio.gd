a3d6471738c85bc15c1c062019a44bb3e0a23cb7
ExpirealBurner <expirealburner@gmail.com>
2026-03-11 23:04:49 +0400

    Lerp and hoverr gng
    added lerp and hover effects to main menu button

--- UI/Finish_Game/ButtonAudio.gd
+++ UI/Finish_Game/ButtonAudio.gd
@@ -1,9 +1,24 @@
 extends Button
 
 @export var soundEffectPress: AudioStreamPlayer2D
+@export var hover_scale: float = 1.1
+@export var lerp_speed: float = 8.0
+
+var target_scale: Vector2 = Vector2.ONE
 
 func _ready():
 	button_down.connect(playSound)
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+
+func _process(delta):
+	scale = scale.lerp(target_scale, lerp_speed * delta)
 
 func playSound():
 	soundEffectPress.play()
+
+func _on_mouse_entered():
+	target_scale = Vector2.ONE * hover_scale
+
+func _on_mouse_exited():
+	target_scale = Vector2.ONE
