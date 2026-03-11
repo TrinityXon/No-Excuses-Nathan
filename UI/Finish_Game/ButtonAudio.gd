8bc691a870e01c13f1b1c848d24ac7df2f77fa0a
Trinity the third of the Xon's. <gameit506@gmail.com>
2026-03-11 22:22:04 +0300

    Lerp button fix
    added an early scale value for, wait for it..... scalability! hilarious

--- UI/Finish_Game/ButtonAudio.gd
+++ UI/Finish_Game/ButtonAudio.gd
@@ -4,12 +4,17 @@ extends Button
 @export var hover_scale: float = 1.1
 @export var lerp_speed: float = 8.0
 
-var target_scale: Vector2 = Vector2.ONE
+
+var early_scale: Vector2
+var target_scale: Vector2
 
 func _ready():
 	button_down.connect(playSound)
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
+	
+	early_scale = self.scale
+	target_scale = early_scale
 
 func _process(delta):
 	scale = scale.lerp(target_scale, lerp_speed * delta)
@@ -18,7 +23,7 @@ func playSound():
 	soundEffectPress.play()
 
 func _on_mouse_entered():
-	target_scale = Vector2.ONE * hover_scale
+	target_scale = early_scale * hover_scale
 
 func _on_mouse_exited():
-	target_scale = Vector2.ONE
+	target_scale = early_scale
